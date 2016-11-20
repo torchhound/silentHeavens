26183bcf20b2d54cf5e5e60173805aca1f28acd9
torchhound <jcieslik@umail.iu.edu>
2016-11-20 18:02:34 -0500

    Added wtforms, going to add a separate form for register, began model for mongodb

--- models.py
+++ models.py
@@ -1,5 +1,29 @@
 import sqlite3
-#from flask.ext.mongoalchemy import MongoAlchemy
+from flask import Flask
+from flask.ext.mongoalchemy import MongoAlchemy
+
+app = Flask(__name__)
+app.config['MONGOALCHEMY_DATABASE'] = 'repository'
+db = MongoAlchemy(app)
+
+class Empire(db.Document):
+	username = db.StringField()
+	
+def addEmpire(username):
+	try:
+		empire = Empire(username = username)
+		empire.save()
+	except Exception as e:
+		print(e)
+		pass
+	
+def removeEmpire(username):
+	try:
+		rm = Empire.query.filter(Empire.username == username).first()
+		rm.remove()
+	except Exception as e:
+		print(e)
+		pass
 
 def insertUser(username, password):
 	conn = sqlite3.connect("users.db")
--- server.py
+++ server.py
@@ -13,23 +13,57 @@ app = Flask(__name__)
 app.secret_key = "echidna"
 bcrypt = Bcrypt(app)
 
+class loginForm(Form):
+	username = TextField("Username",  [validators.Required("Please enter your username.")])
+	#email = TextField("Email", [validators.Required("Please enter your email address."), validators.Email("Please enter your email address.")])
+	password = PasswordField('Password', [validators.Required("Please enter a password.")])
+	register = SubmitField(label="Register")
+	login = SubmitField(label="Login")
+ 
+	def __init__(self, *args, **kwargs):
+		Form.__init__(self, *args, **kwargs)
+ 
+	def validate(self):
+		users = models.retrieveUsers()
+		if not Form.validate(self):
+			return False
+	
+		nameCheck = str(self.username)
+		if nameCheck in users: #check if username has been used before
+			self.username.errors.append("That username is already taken")
+			return False
+		else:
+			return True
+			
+	'''
+		emailCheck = self.email.lower() 
+		if emailCheck in open(dbName, 'r').read(): #check if email has been used before
+			self.email.errors.append("That email is already taken")
+			return False
+		else:
+			return True	
+	'''
+	
 @app.route('/', methods=['POST', 'GET'])
 def login():
-    if request.method=='POST': #move to wtforms
-    	if request.form['submit'] == 'Login':
-    		username = request.form['username']
-        	password = request.form['password']
+	form = loginForm(csrf_enabled = False) #might need to be changed in production
+    if request.method=='POST': 
+    	if form.validate() == False:
+			return render_template("login.html", form=form)
+    	if form.login.data: #split into two forms? One with email and one without
+    		username = form.username.data
+        	password = bcrypt.generate_password_hash(form.password.data)
             users = models.retrieveUsers()
             if username in users and password in users:
         		return render_template('index.html')
         	else:
-        		pass
-        elif request.form['submit'] == 'Register':   
-        	username = request.form['username']
-        	password = request.form['password']
+        		return render_template("login.html", form=form)
+        elif form.register.data:   
+        	username = form.username.data
+        	password = bcrypt.generate_password_hash(form.password.data)
         	models.insertUser(username, password)
     else:
-        return render_template('login.html')
+        return render_template('login.html', form=form)
         
 @app.route('/game', methods=["POST", "GET"])
 def index():
