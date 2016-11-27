58871d8e4d3e2c8dbe1b326f630a5db0fd1c7cd3
torchhound <jcieslik@umail.iu.edu>
2016-11-27 16:21:22 -0500

    Added sticky footer

--- gameServer.py
+++ gameServer.py
@@ -4,9 +4,8 @@ DEBUG = True #change in production
 
 app = Flask(__name__)
 app.secret_key = "echidna"
-bcrypt = Bcrypt(app)
         
-@app.route('/game', methods=["POST", "GET"])
+@app.route('/', methods=["POST", "GET"])
 def index():
 	if request.method == "POST":
 		pass
@@ -14,4 +13,4 @@ def index():
 		return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='127.0.0.1')
