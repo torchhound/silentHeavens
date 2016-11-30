4800d3f30a5823a9d64520ccbc2ba069cd266710
torchhound <unknownmasayoshi@gmail.com>
2016-11-29 23:50:10 -0500

    Removed flask-login

--- authServer.py
+++ authServer.py
@@ -2,7 +2,6 @@ from flask import Flask, request, render_template, redirect, url_for, flash
 from flask_wtf import Form 
 from wtforms import TextField, TextAreaField, SubmitField, validators, ValidationError, PasswordField
 from flask_bcrypt import Bcrypt
-from flask_login import LoginManager, login_required, login_user, logout_user
 import sys
 import models
 import forms
@@ -12,16 +11,7 @@ DEBUG = True #change in production
 app = Flask(__name__)
 app.secret_key = "echidna"
 bcrypt = Bcrypt(app)
-loginManager = LoginManager()
-loginManager.init_app(app)
 
-@loginManager.user_loader
-def load_user(username):
-	try:
-		return models.User.get_id(username)
-	except:
-		return None	
-	
 @app.route('/', methods=['POST', 'GET'])
 def login():
 	loginForm = forms.loginForm(csrf_enabled = False) #needs to be changed in production
@@ -31,11 +21,10 @@ def login():
 			print("Login POST", file=sys.stderr)
 			username = loginForm.username.data
 			password = loginForm.password.data
-			user = models.User.authUser(username, password)
+			user = models.authUser(username, password)
 			if user == True:
 				print("Successful login of {}".format(username), file=sys.stderr)
-				login_user(username) #not sure about that argument
-				return render_template("index.html")
+				#send username and/or token to game server
 			else:
 				print("Failed Login", file=sys.stderr)
 				error = "Failed Login"
@@ -49,7 +38,7 @@ def login():
 			username = registerForm.username.data
 			password = bcrypt.generate_password_hash(registerForm.password.data)
 			email = registerForm.email.data
-			success = models.User.createUser(username, password, email)
+			success = models.insertUser(username, password, email)
 			if success == True:
 				message = "Success!"
 				print("Successful registration of {}".format(username), file=sys.stderr)
--- models.py
+++ models.py
@@ -1,7 +1,6 @@
 import sqlite3
 from flask import Flask
 from flask_mongoalchemy import MongoAlchemy
-from flask_login import UserMixin
 from flask_bcrypt import Bcrypt
 import sys
 
@@ -9,69 +8,6 @@ app = Flask(__name__)
 app.config['MONGOALCHEMY_DATABASE'] = 'repository'
 db = MongoAlchemy(app)
 bcrypt = Bcrypt(app)
-
-class User(UserMixin):
-
-	id = 0
-	username = ""
-	email = ""
-	password = ""
-
-	def __init__(self, username, password, email):
-		self.id = get_id(username)
-		self.username =  username
-		self.password = password
-		self.email = email
-		
-	def createUser(username, password, email):
-		print("create", file=sys.stderr)
-		#insertUser(username, password, email)
-		conn = sqlite3.connect("users.db")
-		cur = conn.cursor()
-		values = [username, password, email]
-		cur.execute("INSERT INTO users (username, password, email) VALUES (?,?,?)", values)
-		conn.commit()
-		conn.close()
-		
-	def authUser(username, password):
-		print("auth", file=sys.stderr)
-		conn = sqlite3.connect("users.db")
-		cur = conn.cursor()
-		auth = cur.execute("SELECT * FROM users") #dislike this current solution, will try and move back to values = [username, password]auth = cur.execute("SELECT * FROM users WHERE username=? AND password=?", values)
-		rows = cur.fetchall()
-		for row in rows:
-			user = row[1]
-			print("user {}".format(user), file=sys.stderr)
-			passw = row[2]
-			print("password {}".format(passw), file=sys.stderr)
-			if user == username and bcrypt.check_password_hash(passw, password):
-				conn.close()
-				return True
-			else:
-				pass
-		conn.close()
-		return False
-		
-	def is_authenticated(self):
-		return True
- 
-	def is_active(self):
-		return True
- 
-	def is_anonymous(self):
-		return False
- 
-	def get_id(self, username):
-		try:
-			conn = sqlite3.connect("users.db")
-			cur = conn.cursor()
-			cur.execute("SELECT id FROM users WHERE username == ?", username)
-			id = cur.fetchall()
-			conn.close()
-			return id
-		except Exception as e:
-			print(e)
-			return False
 
 class Player(db.Document):
 	username = db.StringField()
@@ -93,6 +29,7 @@ def removePlayer(username):
 		pass #return False?
 
 def insertUser(username, password, email):
+	print("create", file=sys.stderr)
 	conn = sqlite3.connect("users.db")
 	cur = conn.cursor()
 	values = [username, password, email]
@@ -107,3 +44,22 @@ def retrieveUsers():
 	users = cur.fetchall()
 	conn.close()
 	return users
+
+def authUser(username, password):
+	print("auth", file=sys.stderr)
+	conn = sqlite3.connect("users.db")
+	cur = conn.cursor()
+	auth = cur.execute("SELECT * FROM users") #dislike this current solution, will try and move back to values = [username, password]auth = cur.execute("SELECT * FROM users WHERE username=? AND password=?", values)
+	rows = cur.fetchall()
+	for row in rows:
+		user = row[1]
+		print("user {}".format(user), file=sys.stderr)
+		passw = row[2]
+		print("password {}".format(passw), file=sys.stderr)
+		if user == username and bcrypt.check_password_hash(passw, password):
+			conn.close()
+			return True
+		else:
+			pass
+	conn.close()
+	return False
